a4190c7cc919ac357f7287ee61c3bcaf8d215d93
listenmarvin <listenmarvin.int@niveussolutions.com>
2023-05-16 10:54:22 +0530

    Updated

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Created a GET api that returns query param, path param and return both values with info as json
 app.get("/:id", (req, res) => {
@@ -9,8 +12,26 @@ app.get("/:id", (req, res) => {
   const response = {
     Id: userId,
     userName: name,
+    
   };
   res.json(response);
+});
+
+
+
+const josnData = [];
+
+app.get("/", (res, req) => {
+  req.json(josnData);
+});
+//  Created a POST api (using THUNDER CLIENT) which acccepts a JSON data and sReturn an array of data.
+app.post("/user", (req, res) => {
+  const newData = {
+    bookId: req.body.bookid,
+    title: req.body.title,
+  };
+  josnData.push(newData);
+  res.json(josnData);
 });
 
 app.listen(8080, () => {
